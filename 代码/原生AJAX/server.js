f8a639183da44255a34c761fb6b3ad06a6c05076
乐乐乐乐培 <LWP13226968149@163.com>
2021-08-30 16:19:49 +0800

    AJAX learning resource

---
+++ 代码/原生AJAX/server.js
@@ -0,0 +1,35 @@
+// 1、引入express
+const { request, response } = require('express');
+const express = require('express');
+
+// 2、创建应用对象
+const app = express();
+
+// 3、创建路由规则
+// request 是对请求报文的封装
+// response 是对相应报文的封装
+app.all('/server',(request,response)=>{
+    // 设置相应头  设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*');
+
+    // 设置响应体
+    response.send('HELLO AJAX - 2');
+});
+
+app.all('/json-server',(request,response)=>{
+    // 设置响应头  设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin','*');
+    // 响应一个数据
+    const data = {
+        name: 'lep101'
+    };
+    // 对对象进行字符串转换
+    let str = JSON.stringify(data);
+    // 设置响应体
+    response.send(str);
+});
+
+// 4、监听端口启动服务
+app.listen(8000,()=>{
+    console.log("服务以及启动,8000 端口监听中....");
+})
